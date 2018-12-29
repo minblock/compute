d3db98c18e8354e5a15c23d4dfb237d2a8e38248
stevecat <38344452+minblock@users.noreply.github.com>
2018-12-29 05:46:01 -0500

    Update gitian-build.sh

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -249,8 +249,6 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/minblock/gitian.sigs.git
-    git clone https://github.com/minblock/ovo-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
