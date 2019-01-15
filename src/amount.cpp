56d91a100c3df7b769c07e415923e2c7185df102
minblock <38344452+minblock@users.noreply.github.com>
2019-01-15 00:00:25 -0500

    Update amount.cpp

--- src/amount.cpp
+++ src/amount.cpp
@@ -7,7 +7,7 @@
 
 #include "tinyformat.h"
 
-const std::string CURRENCY_UNIT = "COMPUTE";
+const std::string CURRENCY_UNIT = "PUTESIES";
 
 CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nBytes_)
 {
