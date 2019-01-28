eed724f5972c20b009bb507fee31a4806e353cf9
stevecat <38344452+minblock@users.noreply.github.com>
2019-01-27 23:58:20 -0500

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Compute");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "0.8"
+#define CLIENT_VERSION_SUFFIX   "-alpha"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
