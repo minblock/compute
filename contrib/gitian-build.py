f1987f0aa15d87de04706694fab1d129a8e428ed
minblock <38344452+minblock@users.noreply.github.com>
2019-02-22 16:19:54 -0500

    Update gitian-build.py

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -24,7 +24,7 @@ def setup():
     subprocess.check_call(['sudo', 'apt-get', 'install', '-qq'] + programs)
 #    if not os.path.isdir('gitian.sigs'):
 #        subprocess.check_call(['git', 'clone', 'https://github.com/minblock/gitian.sigs.git'])
-#    if not os.path.isdir('bitcoin-detached-sigs'):
+#    if not os.path.isdir('compute-detached-sigs'):
 #        subprocess.check_call(['git', 'clone', 'https://github.com/minblock/compute-detached-sigs.git'])
     if not os.path.isdir('gitian-builder'):
         subprocess.check_call(['git', 'clone', 'https://github.com/devrandom/gitian-builder.git'])
@@ -46,13 +46,13 @@ def setup():
 def build():
     global args, workdir
 
-    os.makedirs('bitcoin-binaries/' + args.version, exist_ok=True)
+    os.makedirs('compute-binaries/' + args.version, exist_ok=True)
     print('\nBuilding Dependencies\n')
     os.chdir('gitian-builder')
     os.makedirs('inputs', exist_ok=True)
 
     subprocess.check_call(['wget', '-N', '-P', 'inputs', 'https://downloads.sourceforge.net/project/osslsigncode/osslsigncode/osslsigncode-1.7.1.tar.gz'])
-    subprocess.check_call(['wget', '-N', '-P', 'inputs', 'https://computecore.org/cfields/osslsigncode-Backports-to-1.7.1.patch'])
+    subprocess.check_call(['wget', '-N', '-P', 'inputs', 'https://bitcoincore.org/cfields/osslsigncode-Backports-to-1.7.1.patch'])
     subprocess.check_call(["echo 'a8c4e9cafba922f89de0df1f2152e7be286aba73f78505169bc351a7938dd911 inputs/osslsigncode-Backports-to-1.7.1.patch' | sha256sum -c"], shell=True)
     subprocess.check_call(["echo 'f9a8cdb38b9c309326764ebc937cba1523a3a751a7ab05df3ecc99d18ae466c9 inputs/osslsigncode-1.7.1.tar.gz' | sha256sum -c"], shell=True)
     subprocess.check_call(['make', '-C', '../compute/depends', 'download', 'SOURCES_PATH=' + os.getcwd() + '/cache/common'])
