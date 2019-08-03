0f5b6bcd21751e588feb973a0a7847cc4cb064c9
stevecat <38344452+minblock@users.noreply.github.com>
2019-08-03 13:23:13 -0400

    Copyrights

--- src/bench/crypto_hash.cpp
+++ src/bench/crypto_hash.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2016 The Bitcoin Core developers
-// Copyright (c) 2018 The Compute Core developers
+// Copyright (c) 2018-2019 The Dash Core developers
+// Copyright (c) 2018-2019 The Compute Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
